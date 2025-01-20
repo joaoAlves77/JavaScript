5e9cb196c37733710e21900e70eadd88973fe9ef
João Santana <joao.santana@esoftware.uniceplac.edu.br>
2025-01-20 19:28:16 -0300

    tipo de dados

--- script.js
+++ script.js
@@ -2,8 +2,19 @@
 let hello = "Hello world";
 console.log(nome);
 
+
+// Tipos de declaração de variaveis
+// let e const. 'var' também mas não é mais utilizada.
 // Variaveis
 let nome = "joao";
 let idade = 10;
 const sobreNome = "santana";
 
+//Tipo de dados
+// string, number, boolean, array e object
+let nome2 = "joao"
+let idade2 = 19
+let certo = true
+let lista = ["tomate", "macarrão", "arroz"]
+let NomeIdade = { nome:"joao", idade: 20}
+console.log(typeof nome2) // typeof para saber qual é o tipo da variavel
