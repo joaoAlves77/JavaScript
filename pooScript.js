7fcf96491cc84b71293889a28347c6cb4f466c03
João Santana <joao.santana@esoftware.uniceplac.edu.br>
2025-02-18 18:45:40 -0300

    classes: action

--- pooScript.js
+++ pooScript.js
@@ -1,6 +1,5 @@
 // Objetos e Orientação a Objetos
-// classes: construtor e this
-
+// classes: construtor, this e instâncias
 class Personagem {
     constructor(nome, idade, poder) {
         this.nome = nome;
@@ -9,4 +8,34 @@ class Personagem {
     }
 }
 
-console.log(new Personagem('joao', 20, 'força'));
+console.log(new Personagem('joao', 20, 'força')); // instância
+
+// classes: action
+class Person {
+
+    age = 0;
+    steps = 0;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    takeAstep() {
+        this.steps++;
+    }
+
+    setAge(age) {
+        if(typeof age == 'number') { // verificar se é um número
+            this.age = age;
+        } else {
+            console.log('A idade deve ser um número');
+        }
+    }
+}
+
+let p1 = new Person('João');
+let p2 = new Person('Maria');
+let p3 = new Person('José');
+
+p1.takeAstep();
+console.log(`${p1.name} deu ${p1.steps} passos`);
