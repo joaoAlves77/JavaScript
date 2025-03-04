c997cd08e106bc96da51a000246bfb7e3e7a2a26
João Santana <joao.santana@esoftware.uniceplac.edu.br>
2025-03-04 20:27:08 -0300

    exemplo de request e response

--- requisicao.js
+++ requisicao.js
@@ -1,7 +1,15 @@
+import requests from 'requests';
+
 // O que é Síncrono e Assíncrono?
 // exemplo de sincrono
 let nome = 'joao';
 let sobrenome = 'santana'
 let nomeCompleto = nome + sobrenome;
 
-//
+// exemplo de request e response
+// response = requests.get('https://api.github.com/users/joaosantana');
+
+// // if response.status_code == 200:
+// //     print('Requisição bem sucedida')
+// // else:
+//     print('Requisição falhou')
